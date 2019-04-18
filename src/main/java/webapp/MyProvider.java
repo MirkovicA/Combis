352d25f56e5c895c34fdbca0cfa05d6e85841989
MirkovicA <39918823+MirkovicA@users.noreply.github.com>
2019-04-18 13:21:43 +0200

    Update

--- src/main/java/webapp/MyProvider.java
+++ src/main/java/webapp/MyProvider.java
@@ -3,7 +3,7 @@ package webapp;
 public interface MyProvider {
 	
 	String username = "postgres";
-	String password = "nesta1glupo";
+	String password = "*********";
 	String conUrl = "jdbc:postgresql://localhost:5432/postgres";
 
 }
